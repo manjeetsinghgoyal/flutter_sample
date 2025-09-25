10aa6f5a60a08116d8843097fe157a7807cb5ae5
Manjeet Singh Goyal <manjeetranawat@gmail.com>
2025-09-25 17:19:29 +0530

    update ci cd file

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter CI Demo',
+      title: 'Flutter CI Demo test',
       theme: ThemeData(primarySwatch: Colors.blue),
       home: const CounterScreen(),
     );
