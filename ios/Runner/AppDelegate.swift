c46b390a620eecac6a6f9847f103c85ca53346da
Manjeet Singh Goyal <manjeetranawat@gmail.com>
2025-10-14 19:14:51 +0530

    flutter channels added

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,13 +1,42 @@
-import Flutter
 import UIKit
+import Flutter
 
-@main
+@UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GeneratedPluginRegistrant.register(with: self)
+
+    let controller = window?.rootViewController as! FlutterViewController
+    let batteryChannel = FlutterMethodChannel(name: "com.example.battery",
+                                              binaryMessenger: controller.binaryMessenger)
+
+    batteryChannel.setMethodCallHandler({
+      (call: FlutterMethodCall, result: @escaping FlutterResult) -> Void in
+      if call.method == "getBatteryLevel" {
+        let level = self.getBatteryLevel()
+        if level == -1 {
+          result(FlutterError(code: "UNAVAILABLE",
+                              message: "Battery level not available.",
+                              details: nil))
+        } else {
+          result(level)
+        }
+      } else {
+        result(FlutterMethodNotImplemented)
+      }
+    })
+
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
+
+  private func getBatteryLevel() -> Int {
+    UIDevice.current.isBatteryMonitoringEnabled = true
+    if UIDevice.current.batteryState == UIDevice.BatteryState.unknown {
+      return -1
+    } else {
+      return Int(UIDevice.current.batteryLevel * 100)
+    }
   }
 }
