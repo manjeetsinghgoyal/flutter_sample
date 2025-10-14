c6d3a4c11e3efe591c13dcfcdc3d4612aac3ff0f
Manjeet Singh Goyal <manjeetranawat@gmail.com>
2025-10-14 16:59:51 +0530

    new text added

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Web CI/CD',
+      title: 'Flutter Web CI/CD test for git commands',
       theme: ThemeData(primarySwatch: Colors.blue),
       home: const HomePage(),
     );
